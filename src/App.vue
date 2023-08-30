f4c76e713cbbfa20063261aee7f21285f66b88bb
shadi-ra <shadi_rahbar74@yahoo.com>
2023-08-30 14:38:03 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+<h1>shadi reaction timer</h1>
+<button @click="start" :disabled="isPlaying">play</button>
+<Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+<p v-if="showResult">reactionTime: {{score}} ms</p>
+</template>
+
+<script>
+import Block from './components/BlockComponent.vue'
+import Result from './components/ResultComponent.vue'
+export default {
+  name: 'App',
+  components:{
+    Block
+  },
+data(){
+  return{
+    isPlaying:false,
+    delay:null,
+   score:null,
+   showResult : false
+  }
+},
+methods:{
+  start(){
+    this.delay = 2000 + Math.random()*5000
+    this.isPlaying = true
+this.showResult = false
+  },
+  endGame(input){
+   this.score = input
+   this.isPlaying = false
+   this.showResult = true
+  }
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/BlockComponent.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="block" v-if="showBlock" @click="stopTimer">
+        click me
+    </div>
+
+</template>
+
+<script>
+ export default{
+props:['delay'],
+data(){
+    return{
+        showBlock : false,
+        timer:null,
+        reactiontime:0
+    }
+},
+mounted(){
+
+    setTimeout(() => {
+        this.showBlock = true
+        this.startTimer()
+    }, this.delay);
+},
+methods:{
+    startTimer(){
+             this.timer = setInterval(() => {
+               this.reactiontime +=  10
+             }, 10);
+    },
+    stopTimer(){
+         clearInterval(this.timer) 
+         console.log(this.reactiontime)
+         this.$emit('end',this.reactiontime)
+    }
+}
+// ,
+// updated(){
+//     console.log('updated component')
+// },
+// unmounted(){
+//     console.log('component unmounted')
+// }
+ }
+</script>
+
+<style>
+.block{
+    width:400px;
+    border-radius: 20px;
+    background: #0faf87;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+</style>
